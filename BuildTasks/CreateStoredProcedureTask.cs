b91474c6315faf9439686670ad8d0f5d662cba66
Gerard McParland <gerard.mcparland@live.com>
2017-09-20 22:12:28 +0100

    Add project files.

---
+++ BuildTasks/CreateStoredProcedureTask.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Build.Utilities;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace BuildTasks
+{
+    public class CreateStoredProceduresTask : Task
+    {
+        private string classFileName;
+        public string ClassFileName
+        {
+            get { return classFileName; }
+            set { classFileName = value; }
+        }
+
+        public override bool Execute()
+        {
+            Type t = Type.GetType(classFileName, true, true);
+            PropertyInfo[] props = t.GetProperties();
+            foreach(PropertyInfo p in props)
+            {
+                Debug.Print(p.Name);
+            }
+            return true;
+        }
+    }
+}
